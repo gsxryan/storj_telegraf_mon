9f15ccb3166b1a0bb48d66dcd94500d19f944e10
gsxryan <rcurtisit@gmail.com>
2019-07-14 22:51:33 -0500

    make script silent

--- successrate.sh
+++ successrate.sh
@@ -10,23 +10,14 @@
 LOG="docker logs --since 24h storagenode"
 #LOG="cat /volume1/storj/v3/data/node.log"
 
-
-
 #count of unrecoverable failed audits
 audit_failed_crit=$($LOG 2>&1 | grep GET_AUDIT | grep failed | grep open -c)
-if [ $audit_failed_crit -ge 1 ]
-        then echo CRITICAL: Unrecoverable Failed Audits: $audit_failed_crit
-        fi
 #count of recoverable failed audits`
 audit_failed_warn=$($LOG 2>&1 | grep GET_AUDIT | grep failed | grep -v open -c)
-if [ $audit_failed_warn -ge 1 ]
-        then echo WARNING: Recoverable Failed Audits: $audit_failed_warn
-        fi
 #count of successful audits
 audit_success=$($LOG 2>&1 | grep GET_AUDIT | grep downloaded -c)
 #Ratio of Successful to Failed Audits
 audit_ratio=$(printf '%.3f\n' $(echo "(($audit_success/($audit_failed_crit+$audit_failed_warn+$audit_success))*100)" | bc -l))
-echo Audit Success Rate: $audit_ratio%
 
 #Failed Downloads from your node
 dl_failed=$($LOG 2>&1 | grep '"GET"' | grep failed -c)
@@ -34,7 +25,6 @@ dl_failed=$($LOG 2>&1 | grep '"GET"' | grep failed -c)
 dl_success=$($LOG 2>&1 | grep '"GET"' | grep downloaded -c)
 #Ratio of Failed Downloads
 dl_ratio=$(printf '%.3f\n' $(echo "(($dl_success/($dl_failed+$dl_success))*100)" | bc -l))
-echo Download Success Rate: $dl_ratio%
 
 #count of failed uploads to your node
 put_failed=$($LOG 2>&1 | grep '"PUT"' | grep failed -c)
@@ -42,7 +32,6 @@ put_failed=$($LOG 2>&1 | grep '"PUT"' | grep failed -c)
 put_success=$($LOG 2>&1 | grep '"PUT"' | grep uploaded -c)
 #Ratio of Uploads
 put_ratio=$(printf '%.3f\n' $(echo "(($put_success/($put_failed+$put_success))*100)" | bc -l))
-echo Upload Success Rate: $put_ratio%
 
 #count of failed downloads of pieces for repair process
 get_repair_failed=$($LOG 2>&1 | grep GET_REPAIR | grep failed -c)
@@ -50,7 +39,6 @@ get_repair_failed=$($LOG 2>&1 | grep GET_REPAIR | grep failed -c)
 get_repair_success=$($LOG 2>&1 | grep GET_REPAIR | grep downloaded -c)
 #Ratio of GET_REPAIR
 get_repair_ratio=$(printf '%.3f\n' $(echo "(($get_repair_success/($get_repair_failed+$get_repair_success))*100)" | bc -l))
-echo Repair Download Success Rate: $get_repair_ratio%
 
 #count of failed uploads repaired pieces
 put_repair_failed=$($LOG 2>&1 | grep PUT_REPAIR | grep failed -c)
@@ -58,7 +46,6 @@ put_repair_failed=$($LOG 2>&1 | grep PUT_REPAIR | grep failed -c)
 put_repair_success=$($LOG 2>&1 | grep PUT_REPAIR | grep uploaded -c)
 #Ratio of PUT_REPAIR
 put_repair_ratio=$(printf '%.3f\n' $(echo "(($put_repair_success/($put_repair_failed+$put_repair_success))*100)" | bc -l))
-echo Repair Upload Success Rate: $put_repair_ratio%
 
 #count of concurrent connection max
 concurrent_limit=$($LOG 2>&1 | grep "upload rejected" -c)
