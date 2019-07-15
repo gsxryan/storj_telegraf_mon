3004815153a0b29a62f342b01485b45863330fd7
gsxryan <rcurtisit@gmail.com>
2019-07-14 23:24:06 -0500

    replace bc with awk

--- successrate.sh
+++ successrate.sh
@@ -17,37 +17,41 @@ audit_failed_warn=$($LOG 2>&1 | grep GET_AUDIT | grep failed | grep -v open -c)
 #count of successful audits
 audit_success=$($LOG 2>&1 | grep GET_AUDIT | grep downloaded -c)
 #Ratio of Successful to Failed Audits
-audit_ratio=$(printf '%.3f\n' $(echo "(($audit_success/($audit_failed_crit+$audit_failed_warn+$audit_success))*100)" | bc -l))
+audit_ratio=$(printf '%.3f\n' $(echo -e "$audit_success $audit_failed_crit $audit_failed_warn" | awk '{print ( $1 / ( $1 + $2 + $3 )) * 100 }'))
+
 
 #Failed Downloads from your node
 dl_failed=$($LOG 2>&1 | grep '"GET"' | grep failed -c)
 #count of successful downloads
 dl_success=$($LOG 2>&1 | grep '"GET"' | grep downloaded -c)
 #Ratio of Failed Downloads
-dl_ratio=$(printf '%.3f\n' $(echo "(($dl_success/($dl_failed+$dl_success))*100)" | bc -l))
+dl_ratio=$(printf '%.3f\n' $(echo -e "$dl_success $dl_failed" | awk '{print ( $1 / ( $1 + $2 )) * 100 }'))
 
 #count of failed uploads to your node
 put_failed=$($LOG 2>&1 | grep '"PUT"' | grep failed -c)
 #count of successful uploads to your node
 put_success=$($LOG 2>&1 | grep '"PUT"' | grep uploaded -c)
 #Ratio of Uploads
-put_ratio=$(printf '%.3f\n' $(echo "(($put_success/($put_failed+$put_success))*100)" | bc -l))
+put_ratio=$(printf '%.3f\n' $(echo -e "$put_success $put_failed" | awk '{print ( $1 / ( $1 + $2 )) * 100 }'))
+#Uploads: count of concurrent connection max
+concurrent_limit=$($LOG 2>&1 | grep "upload rejected" -c)
+put_accept_ratio=$(printf '%.3f\n' $(echo -e "$put_success $concurrent_limit" | awk '{print ( $1 / ( $1 + $2 )) * 100 }'))
 
 #count of failed downloads of pieces for repair process
 get_repair_failed=$($LOG 2>&1 | grep GET_REPAIR | grep failed -c)
 #count of successful downloads of pieces for repair process
 get_repair_success=$($LOG 2>&1 | grep GET_REPAIR | grep downloaded -c)
 #Ratio of GET_REPAIR
-get_repair_ratio=$(printf '%.3f\n' $(echo "(($get_repair_success/($get_repair_failed+$get_repair_success))*100)" | bc -l))
+get_repair_ratio=$(printf '%.3f\n' $(echo -e "$get_repair_success $get_repair_failed" | awk '{print ( $1 / ( $1 + $2 )) * 100 }'))
 
 #count of failed uploads repaired pieces
 put_repair_failed=$($LOG 2>&1 | grep PUT_REPAIR | grep failed -c)
 #count of successful uploads of repaired pieces
 put_repair_success=$($LOG 2>&1 | grep PUT_REPAIR | grep uploaded -c)
 #Ratio of PUT_REPAIR
-put_repair_ratio=$(printf '%.3f\n' $(echo "(($put_repair_success/($put_repair_failed+$put_repair_success))*100)" | bc -l))
+put_repair_ratio=$(printf '%.3f\n' $(echo -e "$put_repair_success $put_repair_failed" | awk '{print ( $1 / ( $1 + $2 )) * 100 }'))
 
-#count of concurrent connection max
-concurrent_limit=$($LOG 2>&1 | grep "upload rejected" -c)
+#InfoDB Health Check, disk image is malformed
+infodb_check=#($LOG 2>&1 | grep "disk images is malformed" -c)
 
 echo $(date +'%s'), $audit_ratio, $dl_ratio, $put_ratio, $get_repair_ratio, $put_repair_ratio, $concurrent_limit >> SuccessRatio.log
